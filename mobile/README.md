35146aadfbdfb0ef144b42e7561d4539982a20d0
Luciano Weslen da Silva <luciano.weslen11@gmail.com>
2020-10-15 11:35:51 -0300

    start mobile app

---
+++ mobile/README.md
@@ -0,0 +1,29 @@
+<div align="center">
+  <img src=".github/cover-mobile.svg" />
+</div>
+
+## 🛠️ Techs
+<table>
+  <tr>
+    <td>
+      <img src="https://img.shields.io/badge/react%20native-v12.13.0-FFD666?style=for-the-badge" alt="React Native" />
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="https://img.shields.io/badge/typescript-sdk39.0.3-FFD666?style=for-the-badge" alt="Typescript" />
+    </td>
+  </tr>
+</table>
+
+<br/>
+
+## ▶️ Rodar Aplicação
+Para instalar as dependências que o mobile precisa, rode o comando:
+```bash
+yarn install
+```
+Para rodar use o comando:
+```bash
+yarn start
+```
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+// Screens
+import OrphanageDetails from './views/OrphanageDetails';
+import OrphanagesMap from './views/OrphanagesMap';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
